cd5ffe49ea074bedb9196dc07b8f37d774b8699c
Sergey <Blinks44@yandex.ru>
2024-04-22 16:25:51 +0300

    Update home.vue

--- inertia/pages/home.vue
+++ inertia/pages/home.vue
@@ -8,7 +8,7 @@ defineProps<{ version: number }>()
   <Head title="Homepage" />
 
   <div class="container">
-    <div class="title">AdonisJS {{ version }} x Inertia x Vue.js</div>
+    <div class="title">AdonisJS {{ version }} x Inertia x Vue.js. 182 for the win!</div>
 
     <span>
       Learn more about AdonisJS and Inertia.js by visiting the
